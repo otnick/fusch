4884e4d7e650a4022380fb93471ec8da48f458bd
otnick <116823292+otnick@users.noreply.github.com>
2025-02-17 16:22:25 +0100

    screen bugs

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -6,99 +6,73 @@ export const Canvas = component$(() => {
   const lineWidth = useSignal(3);
 
   // eslint-disable-next-line qwik/no-use-visible-task
-  // https://fusch-backend.onrender.com
-  // 192.168.1.44:3000
-  // https://socket.fusch.fun/
   useVisibleTask$(() => {
     const socket = io("https://socket.fusch.fun/");
     const canvas = document.getElementById("drawingCanvas") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.style.touchAction = "none";
+    const resizeCanvas = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      socket.emit("requestCanvasState");
+    };
+    window.addEventListener("resize", resizeCanvas);
+    resizeCanvas();
 
     let drawing = false;
     let lastX = 0;
     let lastY = 0;
-
-    // Laden des gespeicherten Canvas-Zustands
-    socket.emit("requestCanvasState");
 
     canvas.addEventListener("pointerdown", (e) => {
       drawing = true;
       const rect = canvas.getBoundingClientRect();
       lastX = e.clientX - rect.left;
       lastY = e.clientY - rect.top;
-      drawAndEmit(e);
     });
 
     canvas.addEventListener("pointerup", () => (drawing = false));
+
     canvas.addEventListener("pointermove", (e) => {
       if (!drawing) return;
-      drawAndEmit(e);
-    });
-
-    function drawAndEmit(e: PointerEvent) {
       const rect = canvas.getBoundingClientRect();
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
-      socket.emit("draw", { x, y, color: color.value, lastX, lastY, lineWidth: lineWidth.value });
+      socket.emit("draw", { x, y, lastX, lastY, color: color.value, lineWidth: lineWidth.value });
       drawLine(lastX, lastY, x, y, color.value, lineWidth.value);
       lastX = x;
       lastY = y;
-    }
-
-    socket.on("draw", ({ x, y, lastX, lastY, color, lineWidth }) => drawLine(lastX, lastY, x, y, color, lineWidth));
-
-    socket.on("canvasState", (state) => {
-      const img = new Image();
-      img.src = state;
-      img.onload = () => {
-        if (ctx) ctx.drawImage(img, 0, 0);
-      };
     });
 
-    function drawLine(x1: number, y1: number, x2: number, y2: number, color: string, lineWidth: number) {
-      if (ctx) {
-        ctx.strokeStyle = color;
-        ctx.lineWidth = lineWidth;
-        ctx.lineCap = "round";
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-      }
-    }
+    socket.on("draw", ({ x, y, lastX, lastY, color, lineWidth }) => drawLine(lastX, lastY, x, y, color, lineWidth));
+    socket.on("canvasState", (commands: { x: number, y: number, lastX: number, lastY: number, color: string, lineWidth: number }[]) => {
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      commands.forEach(({ x, y, lastX, lastY, color, lineWidth }) => drawLine(lastX, lastY, x, y, color, lineWidth));
+    });
 
-    // Regelmäßiges Speichern des Canvas-Zustands
-    setInterval(() => {
-      const state = canvas.toDataURL();
-      socket.emit("saveCanvasState", state);
-    }, 1000);
+    function drawLine(x1: number, y1: number, x2: number, y2: number, color: string | CanvasGradient | CanvasPattern, lineWidth: number) {
+      ctx!.strokeStyle = color;
+      ctx!.lineWidth = lineWidth;
+      ctx!.lineCap = "round";
+      ctx!.beginPath();
+      ctx!.moveTo(x1, y1);
+      ctx!.lineTo(x2, y2);
+      ctx!.stroke();
+    }
   });
 
   return (
     <div>
-      <input
-        type="color"
-        class="fixed bottom-6 right-4 z-50 rounded-md"
-        value={color.value}
-        onInput$={(e) => (color.value = (e.target as HTMLInputElement).value)}
-      />
-      <input
-        type="range"
-        min="1"
-        max="20"
-        value={lineWidth.value}
-        class="fixed bottom-7 right-24 z-50"
-        onInput$={(e) => (lineWidth.value = parseInt((e.target as HTMLInputElement).value))}
-      />
-      <canvas
-        id="drawingCanvas"
-        class="w-full h-full fixed top-0 left-0 pointer-events-auto"
-        style="z-index: -1;"
-      ></canvas>
+      <input type="color" class="fixed bottom-6 right-4 z-50" value={color.value} onInput$={(e) => {
+        const target = e.target as HTMLInputElement | null;
+        if (target) {
+          color.value = target.value;
+        }
+      }} />
+      <input type="range" min="1" max="20" value={lineWidth.value} class="fixed bottom-7 right-24 z-50" onInput$={(e) => {
+        const target = e.target as HTMLInputElement;
+        lineWidth.value = parseInt(target.value);
+      }} />
+      <canvas id="drawingCanvas" class="w-full h-full fixed top-0 left-0 pointer-events-auto" style="z-index: -1;"></canvas>
     </div>
   );
 });
