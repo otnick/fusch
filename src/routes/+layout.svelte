6bc3f4fa3e934a4e445eeed7947de14f13532747
nick <116823292+otnick@users.noreply.github.com>
2025-06-15 19:46:21 +0200

    coloring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,21 +16,16 @@
 	<slot />
 </div>
 
-<div class="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 bg-surface-900 shadow-lg rounded-t-xl w-full max-w-md p-3 flex justify-between">
+<div class="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 bg-success-200 shadow-lg rounded-t-xl w-full max-w-2xs p-3 flex justify-between text-black">
 	{#each links as { label, href }, i}
 		<Navigation.Tile
 			id={`tile-${i}`}
 			label={label}
 			href={href}
+			active="bg-success-950 text-white"
 			{...(page.url.pathname === href ? { selected: true } : {})}
 		>
 			<IconBox />
 		</Navigation.Tile>
 	{/each}
 </div>
-
-<!-- <div>
-	<a href="/impressum" class="fixed bottom-0 right-0 m-4 p-2 bg-primary text-on-primary rounded-full shadow-lg hover:bg-primary/90 transition-colors">
-		Impressum
-	</a>
-</div> -->
